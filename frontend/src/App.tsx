1b81061a9c3e55e860ec1773f7924a61e274f87a
rishabh21g <biasedengineer@gmail.com>
2025-04-20 12:50:14 +0530

    routing + lazyloading done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { Suspense } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { lazy } from "react";
+import Loader from "./components/Loader";
+const Home = lazy(()=>import("./pages/Home"));
+const Search = lazy(()=>import("./pages/Search"));
+const Cart = lazy(()=>import("./pages/Cart"));
+
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+  <Suspense fallback={<Loader/>}>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </BrowserRouter>
+  </Suspense>
+  );
+};
 
-export default App
+export default App;
